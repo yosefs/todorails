ee075819da95b9bf37462a68163bfa1d8141e240
yosefs <yosef.s@highgearmedia.com>
2012-08-23 21:52:55 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todorails::Application.config.secret_token = '843a1c90722fbf378867446b57424fcbab7b1c0118491631cb6876387ac05d6ed9d33f5fa65c1804f4bf68a591980c9ca3d90032fb7bc1d718a9ff4982fd35f0'
