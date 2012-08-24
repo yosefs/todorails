1da2962e5e8e8a4a9735b2484cfb4d9b682f2302
yosefs <yosef.s@highgearmedia.com>
2012-08-24 14:24:56 +0300

    add backbone

---
+++ app/assets/javascripts/todorails.js
@@ -0,0 +1,13 @@
+window.Todorails = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Todorails.init();
+});
